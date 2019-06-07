5936de130d955b9aa3fce810a7d946cc0b4f237f
Erin Stalzer <erinstalzer@gmail.com>
2019-06-07 18:30:35 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,24 +2,7 @@
 
 class Article < ActiveRecord::Base
 
-  @@all = []
-
   attr_accessor :title, :content, :id
 
-
-
-  def self.create(hash)
-    article = Article.new
-    article.title = hash[:title]
-    article.content = hash[:content]
-    #article.id = Article.maximum(:id).next
-    @@all << article
-    article.save
-    article
-  end
-
-  def self.all
-    @@all
-  end
 
 end
