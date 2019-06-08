334bf44707010d08ca35d54df2481cd2e8e6d477
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-06-08 04:34:50 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,18 +2,6 @@
 
 class Article < ActiveRecord::Base
   
-  attr_accessor :title, :content 
-  
-  @@all = [] 
-  
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content] 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
+ 
   
 end
